e4344051d20b699817c6fdaa62050bd2534ce2c1
Algo Hussle <citizensofandrometta@gmail.com>
2023-07-01 09:52:35 -0500

    initial commit

---
+++ .history/script_20230701090956.js
@@ -0,0 +1,28 @@
+setInterval(function () {
+    var today = new Date();
+    var clk = $("#clock");
+    var hora =
+      today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    clk.text(hora);
+    $("#stat").toggleClass("led-off");
+  }, 500);
+  
+  function pra8(n){
+    if (n > 0) return Math.ceil(n / 8.0) * 8;
+    else if (n < 0) return Math.floor(n / 8.0) * 8;
+    else return 8;
+  }
+  
+  var rds = "CODEPEN FM - YOUR KNOWLEDGE BASE FOR DEVELOPMENT IMPROVEMENT";
+  var idx_ini = 0;
+  var idx_end =  rds.length;
+  
+  setInterval(function () {
+    var display = rds.substring(idx_ini, idx_ini+7 );
+    if ((idx_ini + 7) >= rds.length){
+      idx_ini = 0;  
+    }
+    idx_ini++;
+    
+    $("#rds").text(display);  
+  }, 500);
